a612062a6e651d21ca11f8007a5bde88679da8b5
Rath Rene <rath@oxl.at>
2024-09-18 18:27:28 +0200

    Revert "update ip-info lookup"
    This reverts commit 08fa9f5071f11dfc9e5c13fdeb77066d4296df75.

--- lua/geoip_lookup_w_backend.lua
+++ lua/geoip_lookup_w_backend.lua
@@ -2,7 +2,9 @@
 -- Copyright (C) 2024 Rath Pascal
 -- License: MIT
 
-local function http_request(lookup, filter, src)
+-- NOTE: the ltrim parameter can be used to remove a prefix - like: 'AS1337' => '1337'
+
+local function http_request(lookup, filter, src, ltrim)
     local s = core.tcp()
 
     local addr = '127.0.0.1'
@@ -33,40 +35,40 @@ local function http_request(lookup, filter, src)
     if res_body == nil then
         return '00'
     end
-    return string.sub(res_body, 1, -2)
+    return string.sub(res_body, 1 + ltrim, -2)
 end
 
 -- examples for MaxMind:
 
 local function lookup_geoip_country(txn)
-    country_code = http_request('country', 'country.iso_code', txn.f:src())
+    country_code = http_request('country', 'country.iso_code', txn.f:src(), 0)
     txn:set_var('txn.geoip_country', country_code)
 end
 
 local function lookup_geoip_asn(txn)
-    asn = http_request('asn', 'autonomous_system_number', txn.f:src())
+    asn = http_request('asn', 'autonomous_system_number', txn.f:src(), 0)
     txn:set_var('txn.geoip_asn', asn)
 end
 
 local function lookup_geoip_asname(txn)
-    asname = http_request('asn', 'autonomous_system_organization', txn.f:src())
+    asname = http_request('asn', 'autonomous_system_organization', txn.f:src(), 0)
     txn:set_var('txn.geoip_asname', asname)
 end
 
 -- examples for IPInfo:
 
 local function lookup_geoip_country(txn)
-    country_code = http_request('country', 'country', txn.f:src())
+    country_code = http_request('country', 'country', txn.f:src(), 0)
     txn:set_var('txn.geoip_country', country_code)
 end
 
 local function lookup_geoip_asn(txn)
-    asn = http_request('asn', 'asn_numeric', txn.f:src())
+    asn = http_request('asn', 'asn', txn.f:src(), 2)
     txn:set_var('txn.geoip_asn', asn)
 end
 
 local function lookup_geoip_asname(txn)
-    asname = http_request('asn', 'name', txn.f:src())
+    asname = http_request('asn', 'name', txn.f:src(), 0)
     txn:set_var('txn.geoip_asname', asname)
 end
 
